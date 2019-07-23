5e025ce6f6daa441cbdf1fe2f402fb3b597c087b
chaitanyarahalkar <chaitanyarahalkar4@gmail.com>
2019-07-23 11:22:58 +0530

    Update init script

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+pacman-key --init
+pacman-key --populate
+
+# Attempt to work around build failure on debian hosts.
+mkdir -p /build/archiso/work/x86_64/airootfs/run/shm
+mkdir -p /build/archiso/work/x86_64/airootfs/var/run/shm
+mkdir -p /run/shm
+mkdir -p /var/run/shm
+
 set -e -u
 
 sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
@@ -19,5 +28,35 @@ sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
+groupadd frost
+groupadd autologin
+useradd -g frost -d /home/frost -m -s /bin/zsh  -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel,docker,autologin" frost
+passwd -d frost
+echo "frost ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers;
+
+# Branding
+sed -i.bak 's/Arch Linux/Frost OS/g' /usr/lib/os-release
+sed -i.bak 's/arch/frost/g' /usr/lib/os-release
+sed -i.bak 's#www.archlinux.org#github.com/chaitanyarahalkar/frostOS#g' /usr/lib/os-release
+sed -i.bak 's#bbs.archlinux.org#github.com/chaitanyarahalkar/frostOS#g' /usr/lib/os-release
+sed -i.bak 's#bugs.archlinux.org#github.com/chaitanyarahalkar/frostOS#g' /usr/lib/os-release
+
+
+
 systemctl enable pacman-init.service choose-mirror.service
-systemctl set-default multi-user.target
+systemctl enable sddm.service
+systemctl enable graphical.target
+
+# Enable the Network Manager
+systemctl enable NetworkManager
+
+# Enable Bluetooth
+systemctl enable bluetooth.service
+
+# Enable NTP Daemon
+systemctl enable ntpd.service
+
+# Enable Cron
+systemctl enable cronie.service
+
+systemctl set-default graphical.target
